76c447b9e6a77e53ffe7b92ebb38276706f74fc1
bernardfernando <jagathf@gmail.com>
2023-08-18 09:48:25 +0100

    amend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+export default async function Home() {
+  const res = await fetch(
+    "https://api.github.com/repos/bernardfernando/page-counter",
+    { next: { revalidate: 10 } }
   );
+  const data = await res.json();
+
+  return <main>({data.stargazers_count})</main>;
 }
